23a331505e454d4431ef8be475cb5ab7ca65a2e1
MrMcLaine <Lypovskyi@gmail.com>
2023-04-19 13:07:10 +0300

    added findByAdmin method for ApllicationForAdmissionRepository

--- system/src/main/java/ua/admissions/system/repository/ApplicationForAdmissionRepository.java
+++ system/src/main/java/ua/admissions/system/repository/ApplicationForAdmissionRepository.java
@@ -2,6 +2,10 @@ package ua.admissions.system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.admissions.system.entity.ApplicationForAdmission;
+import ua.admissions.system.entity.person.Admin;
+
+import java.util.List;
 
 public interface ApplicationForAdmissionRepository extends JpaRepository<ApplicationForAdmission, Long> {
+    List<ApplicationForAdmission> findByAdmin(Admin admin);
 }
